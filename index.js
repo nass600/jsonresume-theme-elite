ab9db85ca299d9a9d112586d81a9f20f5de6dce7
Ignacio Velazquez <ivelazquez85@gmail.com>
2018-07-15 22:36:34 +0100

    Reverted helpers to the main index file (#4)

--- index.js
+++ index.js
@@ -3,12 +3,59 @@ const path = require('path')
 const handlebars = require('handlebars')
 const handlebarsWax = require('handlebars-wax')
 const Swag = require('swag')
-const helpers = require('./src/js/helpers')
+const pluralize = require('pluralize')
+const moment = require('moment')
 const config = require('./build/config')
 const themeConfig = require('./theme-config')
 
 Swag.registerHelpers(handlebars)
-handlebars.registerHelper(helpers)
+handlebars.registerHelper({
+  formatDate: function (date) {
+    if (typeof date === 'undefined') {
+      return 'now'
+    }
+    return moment(date).format('MMM YYYY')
+  },
+  dateDiff: function (startDate, endDate) {
+    let text = ''
+    startDate = moment(startDate)
+    endDate = moment(endDate)
+    let years = endDate.diff(startDate, 'years')
+    startDate.add(years, 'years')
+    let months = endDate.diff(startDate, 'months')
+
+    if (years > 0) {
+      text += `${years} ${pluralize('years', years)}`
+    }
+    if (months > 0) {
+      if (years > 0) {
+        text += ' '
+      }
+      text += `${months} ${pluralize('months', months)}`
+    }
+
+    return text
+  },
+  dateAgo: function (date) {
+    return moment(date, 'YYYY-MM-DD').fromNow()
+  },
+  removeSchema: function (url) {
+    return url.replace(/^.*:\/\//i, '')
+  },
+  sanitizeLowercase: function (str) {
+    return str.replace(/[^a-z0-9]/gi, '-').toLowerCase()
+  },
+  times: function (n, block) {
+    let accum = ''
+    let i = 0
+
+    while (++i <= n) {
+      accum += block.fn(i)
+    }
+
+    return accum
+  }
+})
 
 function render (resume, pageFormat) {
   let css = fs.readFileSync(path.join(__dirname, config.paths.styles.entry), 'utf-8')
